aa121ce463bc1212387dfb7c7f0fbcd719f60b54
allan <allanrey.macasaquit@gmail.com>
2023-12-02 14:00:09 -0500

    Lesson 2. Set up Middleware

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import expressHandlebars from 'express-handlebars';
 import nodemailer from 'nodemailer';
 
 const app = express();
+//View Engine Setup
+app.engine('handlebars', expressHandlebars);
+app.set('view engine', 'handlebars');
+
+//Body-Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello');
